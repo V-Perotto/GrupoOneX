63aed7c44204eafbb70f851303664805829b8d3c
ViperHV <trubisco.hv@outlook.com>
2020-04-27 15:47:17 -0300

    Adicionando pastas [js e css] com seus respectivos arquivos [script.js e blog.css] e [about.html e home.html] na pasta [blog\static], forms.py, [about.html, base.html, post_detail.html e post_edit.html] na pasta [blog\template\blog]; modificados os arquivos [urls.py e views.py] com html static e base/header

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from .forms import PostForm
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import redirect
+from django.http import HttpResponse
+
+def about(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    search = request.GET.get('search')
+
+    if search:
+        posts = Post.objects.filter(title__icontains=search)
+    else:
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post', post})
 # Create your views here.
-<<<<<<< HEAD
-# Arthur
-=======
-# Loraine
->>>>>>> c33eb7f5314650b04be6348e7dbd3cd56ca34b7a
+
+@login_required
